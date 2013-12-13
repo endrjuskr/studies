db69f992311546d74ed6b8c87cd7a3f8d85631e4
Andrzej Skrodzki <andrzejskrodzki@andrzejs-air.home>
2013-12-13 20:39:20 +0100

    Add jasmin.jar.

---
+++ MRJP/LatteCompilerPython/lib/Runtime.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Runtime {
+	private static Scanner scanner = new Scanner(System.in);
+	private static String errorMsg = "runtime error"; 
+	public static int readInt()	{
+		String s = scanner.nextLine();
+		return Integer.parseInt(s);
+	}
+	
+	public static String readString() {
+		return scanner.nextLine();
+	}
+	
+	public static void printInt(int k) {
+		System.out.println(k);
+	}
+	
+	public static void printString(String k) {
+		System.out.println(k);
+	}
+	
+	public static void error() {
+		System.out.println(errorMsg);
+		System.exit(1);
+	}
+	
+	public static String concatenate(String a, String b) {
+		return a + b;
+	}
+}
