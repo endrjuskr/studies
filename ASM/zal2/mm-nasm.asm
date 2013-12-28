05a7865e376be433e40cf1436d4e660cea516519
Andrzej Skrodzki <andrzejskrodzki@Andrzejs-MacBook-Air.local>
2013-12-28 01:45:25 +0100

    Multiple small matrix

--- ASM/zal2/mm-nasm.asm
+++ ASM/zal2/mm-nasm.asm
@@ -28,37 +28,59 @@ optimal_mm:
 	mov ebx, [esp + 20]
 	mov dword [addr_c], ebx  ; czwarty argument
 
-	mov ebx, dword [addr_a]
+	mov ecx, 0
+for:
+	mov eax, dword [size]
+	imul eax, dword [size]
+	cmp eax, ecx
+	je end 						; after filling whole matrix (size * size) jump to end
+
+clear_cell:
+	mov eax, dword [addr_c]
+	mov ebx, ecx
+	imul ebx, 4
+	add eax, ebx
+	mov ebx, 0
+	mov [eax], ebx
+
+	mov eax, ecx
+	mov edx, 0
+	mov ebx, dword [size]
+	div ebx				; eax determines which row, edx which column
+
+	mov edi, dword [size]
+	imul edi, 4
+
+add_vec:
+	sub edi, 16
+	mov ebx, dword [addr_b]
+	imul eax, 4
+	imul eax, dword [size]
+	add ebx, eax
+	add ebx, edi
 	movaps xmm0, [ebx]
 
-	mov ecx, 0
-
-	mov eax, dword [addr_b]
-	add ecx, eax
-	mov ebx, [ecx]
-	mov [tmp], ebx
+	mov ebx, dword [addr_a]
+	add ebx, edi
+	imul edx, 4
+	add ebx, edx
+	mov eax, [ebx]
+	mov [tmp], eax
 
 	mov eax, dword [size]
 	shl eax, 2
-	add ecx, eax
-	mov eax, dword [addr_b]
-	mov ebx, [ecx]
-	mov [tmp + 4], ebx
+	add ebx, eax
+	mov edx, [ebx]
+	mov [tmp + 4], edx
 	
-	mov eax, dword [size]
-	shl eax, 2
-	add ecx, eax
-	mov eax, dword [addr_b]
-	mov ebx, [ecx]
-	mov [tmp + 8], ebx
+	add ebx, eax
+	mov edx, [ebx]
+	mov [tmp + 8], edx
 	
-	mov eax, dword [size]
-	shl eax, 2
-	add ecx, eax
-	mov eax, dword [addr_b]
-	mov ebx, [ecx]
-	mov [tmp + 12], ebx
-
+	add ebx, eax
+	mov edx, [ebx]
+	mov [tmp + 12], edx
+	
 
 	movups xmm1, [tmp]
 	mulps  xmm0, xmm1
@@ -66,7 +88,20 @@ optimal_mm:
 	haddps xmm0, xmm0
 	haddps xmm0, xmm0
 
+	mov eax, dword [addr_c]
+	mov ebx, ecx
+	imul ebx, 4
+	add eax, ebx
+
 	movss dword [wynik], xmm0
-	mov eax, dword [wynik]
+	mov ebx, dword [wynik]
+	add [eax], ebx
+
+	cmp edi, 0
+	jne add_vec
+	
+	add ecx, 1
+	jmp for
+end:
 	leave
 	ret
