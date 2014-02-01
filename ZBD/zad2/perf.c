3f5e597e2578c151b0a273f894e173ada6bb40bd
Andrzej Skrodzki <andrzej.skrodzki@gmail.com>
2014-02-01 20:03:02 +0100

    ZBD

--- ZBD/zad2/perf.c
+++ ZBD/zad2/perf.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <sys/time.h>
 #include <stdlib.h>
+#include <time.h>
+
 #define BLOCK_SIZE 1024 * 8 // rozmiar bloku, czyli 8kB
 
 #define READ 1
@@ -12,8 +14,7 @@ int main(int argc, char** argv)
 	// Podajemy w pierwszym argumencie nazwe pliku.
 	// W drugim podajemy liczbe testow.
 	// W trzecim podajemy rozmiar pliku w bajtach.
-	// W czwartym podajemy rozmiar bloku, ktory wczytujemy
-	if (argc != 5)
+	if (argc != 4)
 	{
 		printf("Wrong usage.\n");
 		return -1;
@@ -29,39 +30,39 @@ int main(int argc, char** argv)
 		return -1;
 	}
 
-	int block_count = atoi(argv[4]);
+	fseek(fd, 0L, SEEK_END);
+	int file_size = ftell(fd);
+
+	int block_count = atoi(argv[3]);
 	srand (time(NULL));
 	data = (char*)malloc(sizeof(char) * BLOCK_SIZE * block_count);
 
-	int test_count = atoi(argv[2]);
-	int file_size = atoi(argv[3]);
 	file_size -= block_count * BLOCK_SIZE;
+
+	int test_count = atoi(argv[2]);
 	int operation_number_in_test;
 	int i, picked_block;
 	for(i = 1; i <= test_count; i++)
 	{
 		operation_number_in_test = 0;
-		int milsec = 0;
-		int t;
-		do
+		int t = 200000;
+		gettimeofday(&t1, NULL);
+		while (t--)
 		{
-			operation_number_in_test++;gettimeofday(&t1, NULL);
-			t = 1000;
-			while (t--)
-			{
-				picked_block = drand48();
-				fseek(fd, picked_block, SEEK_SET);
-				//#ifdef READ
-				fgets(data, BLOCK_SIZE * block_count, fd);
-			//#else
-			//fputs(data, fd);
-			//#endif
-			}
-			printf("1\n");
-			gettimeofday(&t2, NULL);
-			milsec += (t2.tv_sec - t1.tv_sec) * 1000000 + (t2.tv_usec - t1.tv_usec);
-		} while (milsec < 1000000);
-		printf("Test %d - %d operations.\n", i, operation_number_in_test);
+			picked_block = drand48();
+			fseek(fd, picked_block, SEEK_SET);
+			#ifdef READ
+			fgets(data, BLOCK_SIZE * block_count, fd);
+			#else
+			fgets(data, fd);
+			#endif
+			operation_number_in_test++;
+		}
+
+		gettimeofday(&t2, NULL);
+		double sec_diff = (t2.tv_sec - t1.tv_sec) * 1000 + (t2.tv_usec - t1.tv_usec) / 1000;
+		printf("%lf\n", sec_diff);
+		printf("Test %d - %d operations.\n", i, (int)((double)operation_number_in_test / sec_diff));
 	}
 	
 	fclose(fd);
